91526445679d6c68eb47e3fd1106c8b5c809eb09
Marek Sęk <marek.sek@gmail.com>
2017-06-05 09:39:20 +0200

    added main SCSS file

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
