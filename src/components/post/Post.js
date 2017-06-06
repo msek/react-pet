f0fa8b833ce62f479a46694c71d10ff4df137d9d
Marek Sęk <marek.sek@gmail.com>
2017-06-06 14:23:53 +0200

    styling for post edit/delete buttons

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -6,11 +6,18 @@ class Post extends React.Component {
     return (
       <div className="panel panel-default">
         <div className="panel-body">
-          <h4>{this.props.title}</h4>
-          <p>{this.props.body}</p>
-          <Link to={'/post/'+this.props.id}>
-            <button type="button" className="btn btn-default pull-right">Edit</button>
-          </Link>
+          <div className="row">
+            <div className="col-sm-10">
+              <h4>{this.props.title}</h4>
+              <p>{this.props.body}</p>
+            </div>
+            <div className="col-sm-2">
+              <Link to={'/post/'+this.props.id}>
+                <button type="button" className="btn btn-default btn-post-list">Edit</button>
+              </Link>
+              <button type="button" className="btn btn-default btn-post-list">Delete</button>
+            </div>
+          </div>
         </div>
       </div>
     );
