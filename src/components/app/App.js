fd3ed0848cc095508f9e13ae1e719ee65ad84ea8
Marek Sęk <marek.sek@gmail.com>
2017-06-08 14:43:17 +0200

    changed import order

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Route, { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../header/Header';
 import HomePage from '../homepage/HomePage';
 import PostEditPage from '../posteditpage/PostEditPage';
