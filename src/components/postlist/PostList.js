9065bccba7a77a290e83f3e0222b2557176e0408
Marek Sęk <marek.sek@gmail.com>
2017-06-20 13:49:17 +0200

    added post delete button

--- src/components/postlist/PostList.js
+++ src/components/postlist/PostList.js
@@ -5,14 +5,18 @@ import 'whatwg-fetch';
 
 export default class PostList extends Component {
   static propTypes = {
-    posts: PropTypes.array
+    posts: PropTypes.array.isRequired,
+    deletePost: PropTypes.func.isRequired
   };
 
   render() {
     return (
       <div className="row">
         <div className="col-xs-12">
-          {this.props.posts.map(post => <Post key={post.id} id={post.id} title={post.title} body={post.body} />)}
+          {this.props.posts.map(post =>
+            <Post key={post.id} id={post.id} title={post.title} body={post.body}
+                  deletePost={this.props.deletePost} />
+          )}
         </div>
       </div>
     );
